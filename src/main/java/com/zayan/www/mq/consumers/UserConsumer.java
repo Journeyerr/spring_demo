cef2d30e387d4dd87ca4c07069c1f23abd853757
AnYuan <zhanganyuan@heytea.com>
2020-04-06 21:13:51 +0800

    修复读取mq的消息体

--- src/main/java/com/zayan/www/mq/consumers/UserConsumer.java
+++ src/main/java/com/zayan/www/mq/consumers/UserConsumer.java
@@ -3,13 +3,12 @@ package com.zayan.www.mq.consumers;
 import com.alibaba.fastjson.JSONObject;
 import com.zayan.www.constant.enums.RabbitMqMessageConstant;
 import com.zayan.www.model.entity.User;
-import com.zayan.www.mq.UserMq;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 /**
@@ -27,10 +26,9 @@ public class UserConsumer {
             value = @Queue(value = RabbitMqMessageConstant.USER_QUEUE, durable = "true"),
             exchange = @Exchange(value = RabbitMqMessageConstant.USER_EXCHANGE)
     ))
-    public void onMassageUser(byte[] bytes) {
-
-        User user = JSONObject.parseObject(new String(bytes), User.class);
-
-        System.out.println("-------开始消费：name = " + user.getName());
+    public void onMassageUser(Message message) {
+        log.info("message--------{}", message);
+        User user = JSONObject.parseObject(new String(message.getBody()), User.class);
+        log.info("开始消费======={}", user.getName());
     }
 }
