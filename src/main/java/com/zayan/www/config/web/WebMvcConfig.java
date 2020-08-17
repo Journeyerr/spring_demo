feb01874bd11d848461094cf6355cbc0f596c3b9
AnYuan <Journeyerrr@gmail.com>
2020-08-17 22:38:24 +0800

    设置静态文件访问路径

--- src/main/java/com/zayan/www/config/web/WebMvcConfig.java
+++ src/main/java/com/zayan/www/config/web/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.zayan.www.config.web;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080");
+    }
+
+    /**
+     * 配置Resource文件夹下面图片可访问
+     * addResourceHandler 设置访问路径
+     * addResourceLocations 设置文件相对路径
+     *
+     * @param registry registry
+     */
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/images/");
     }
 }
