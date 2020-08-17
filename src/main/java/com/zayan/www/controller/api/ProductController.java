c937152daf10d3ec4600d69a4967151d34a427e6
AnYuan <Journeyerrr@gmail.com>
2020-08-18 00:59:11 +0800

    创建商品控制器

---
+++ src/main/java/com/zayan/www/controller/api/ProductController.java
@@ -0,0 +1,11 @@
+package com.zayan.www.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/product")
+public class ProductController {
+
+
+}
