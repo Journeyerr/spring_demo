a82ea796194c2ce2549dc4df76377d9e55852029
AnYuan <Journeyerrr@gmail.com>
2020-08-27 18:19:07 +0800

    token拦截器

--- src/main/java/com/zayan/www/config/web/TokenInterceptor.java
+++ src/main/java/com/zayan/www/config/web/TokenInterceptor.java
@@ -2,6 +2,7 @@ package com.zayan.www.config.web;
 
 import com.zayan.www.util.RequestUtil;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,5 +17,23 @@ public class TokenInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         Integer userIdFormContextToken = RequestUtil.getUserIdFormContextToken();
         return Objects.nonNull(userIdFormContextToken);
+
+        //如果设置为false时，被请求时，拦截器执行到此处将不会继续操作
+        //如果设置为true时，请求将会继续执行后面的操作
+    }
+    /**
+     * 请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+//         System.out.println("执行了TestInterceptor的postHandle方法");
+    }
+
+    /**
+     * 在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+//        System.out.println("执行了TestInterceptor的afterCompletion方法");
     }
 }
